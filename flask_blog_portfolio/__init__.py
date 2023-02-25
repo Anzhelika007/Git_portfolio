c46656874a79303383f9f77ce51c2aab979c76bb
Anzhalika Turlak <293852647@mail.ru>
2023-02-25 11:39:49 +0300

    add register blueprint main

--- flask_blog_portfolio/__init__.py
+++ flask_blog_portfolio/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_blog_portfolio.config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -9,4 +10,11 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__)
     db.init_app(app)
+
+    #Application Registration
+    from flask_blog_portfolio.main.routes import main
+    app.register_blueprint(main)
+
+    app.config.from_object(Config)
+
     return app
--- flask_blog_portfolio/main/routes.py
+++ flask_blog_portfolio/main/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, Blueprint
 
 main = Blueprint('main', __name__)
 
+
 @main.route("/")
 @main.route("/home")
 def home():
