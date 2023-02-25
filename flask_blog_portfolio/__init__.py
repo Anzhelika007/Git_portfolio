5e1b2ddaba06f2e45ea3457402510cb7115ff668
Anzhalika Turlak <293852647@mail.ru>
2023-02-25 10:55:21 +0300

    add models post and user

--- flask_blog_portfolio/__init__.py
+++ flask_blog_portfolio/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 
 def create_app():
-    print(__name__)
     app = Flask(__name__)
+    db.init_app(app)
     return app
