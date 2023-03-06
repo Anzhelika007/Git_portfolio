97e4a400df1909d8ac7a0b55e9e825367e37f35a
Anzhalika Turlak <293852647@mail.ru>
2023-03-06 11:00:26 +0300

    fix bag template home

--- flask_blog_portfolio/main/routes.py
+++ flask_blog_portfolio/main/routes.py
@@ -5,6 +5,8 @@ from flask_blog_portfolio.models import Post, Comment, Like, Category, Hashtag, 
 from flask_blog_portfolio.posts.forms import PostForm, CommentForm, LikeForm
 from flask_blog_portfolio.users.utils import save_picture
 
+from random import shuffle
+
 main = Blueprint('main', __name__)
 
 
@@ -14,10 +16,16 @@ def home():
     posts_first = Post.query.order_by(Post.date_posted.desc()).first_or_404()
     posts = Post.query.order_by(Post.date_posted.desc()).all()
     categories = Category.query.all()
+    categories_head = []
+    posts_head = []
 
     for i in categories:
-        count = Post.query.filter_by(category_id=i.id).count()
-        print(i.name, count)
-    print(categories)
-    print(posts_first.title)
-    return render_template('home.html', posts_first=posts_first, categories=categories, posts=posts)
+        count = Post.query.filter_by(category_id=i.id)
+        if count.count() > 3:
+            posts_head.extend(count[:3])
+            categories_head.append(i)
+
+    shuffle(categories_head)
+
+    return render_template('home.html', posts_first=posts_first, categories_head=categories_head[:3],
+                           posts_head=posts_head, posts=posts)
